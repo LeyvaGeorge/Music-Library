8f23a57520c4c6411da2af6c43e1b30e574ea485
George Leyva <leyvageorge@hotmail.com>
2023-04-18 19:31:00 -0700

    changes as day 6

---
+++ src/components/ArtistView.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { useParams, Link , useNavigate} from "react-router-dom";
+
+export default function ArtistView() {
+    const { id } = useParams()
+    const [artistData, setArtistData] = useState([])
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const API_URL = `http://localhost:4000/album/${id}`;
+            const response = await fetch(API_URL);
+            const resData = await response.json();
+            setArtistData(resData.results);
+
+        }
+        fetchData();
+    });
+
+    const navButtons = () => {
+        return(
+            <div>
+                <button type="button" onClick={ () => navigate(-1) }>Back</button>
+
+                <button type="button" onClick={ () => navigate('/')}>Home</button>
+            </div>
+        )
+    }
+
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album');
+
+    const renderAlbums = justAlbums.map((album, index) => {
+        return (
+            <div key={index}>
+                <Link to={ `/album/${ album.collectionId }` }>
+                   <p>{album.collectionName}</p> 
+                </Link>
+                
+            </div>
+        )
+
+    });
+
+    return (
+        <div>
+            {artistData.length >  0 ? <h2>{artistData[0].artistName}</h2> : <h2>Loading . . .</h2>}
+            { navButtons() }
+            { renderAlbums }
+        </div>
+    )
+}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,25 +1,24 @@
 import { useContext } from "react";
 import { SearchContext } from "../contexts/SearchContext";
 
-
 export default function SearchBar() {
-    const {term,handleSearch} = useContext(SearchContext)
+    const { term, handleSearch } = useContext(SearchContext);
 
     let onSearchSubmit = e => {
         e.preventDefault()
         handleSearch(term.current.value)
+        term.current.value = ''
     }
-    return (
-        <form
-            onSubmit={ onSearchSubmit }>
-            <input 
-                type="text" 
+
+    return(
+        <form onSubmit={ onSearchSubmit }>
+            <input
+                type="text"
                 ref={term}
                 placeholder="Enter a search term here"
             />
-            <input type="submit" />
+            <input type="submit"/>
         </form>
-
     )
 
 }
